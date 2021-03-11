6db0735ab84096825f509e0a2dcf819597e6cac0
shengbinxin <shengbinxin@inibiru.com>
2021-03-11 19:38:10 +0800

    更新脚本

--- jd.sh
+++ jd.sh
@@ -2,15 +2,17 @@
 
 ## 路径
 ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
-[ ${JD_DIR} ] && HelpJd=jd || HelpJd=jd.sh
+[[ ${JD_DIR} ]] && HelpJd=jd || HelpJd=jd.sh
+[[ ${JD_DIR} ]] && ShellJd=jd || ShellJd=${ShellDir}/jd.sh
 ScriptsDir=${ShellDir}/scripts
 ConfigDir=${ShellDir}/config
-OtherScriptsDir=""
 FileConf=${ConfigDir}/config.sh
 FileConfSample=${ShellDir}/sample/config.sh.sample
 LogDir=${ShellDir}/log
 ListScripts=($(cd ${ScriptsDir}; ls *.js | grep -E "j[drx]_|my_"))
 ListCron=${ConfigDir}/crontab.list
+ListCronLxk=${ScriptsDir}/docker/crontab_list.sh
+ListJs=${LogDir}/js.list
 
 ## 导入config.sh
 function Import_Conf {
@@ -43,74 +45,55 @@ function Count_UserSum {
 ## 组合Cookie和互助码子程序
 function Combin_Sub {
   CombinAll=""
-  for ((i=1; i<=${UserSum}; i++)); do
-    for num in ${TempBlockCookie}; do
-      if [[ $i -eq $num ]]; then
-        continue 2
-      fi
+  if [[ ${AutoHelpOther} == true ]] && [[ $1 == ForOther* ]]; then
+
+    ForOtherAll=""
+    MyName=$(echo $1 | perl -pe "s|ForOther|My|")
+
+    for ((m=1; m<=${UserSum}; m++)); do
+      TmpA=${MyName}$m
+      TmpB=${!TmpA}
+      ForOtherAll="${ForOtherAll}@${TmpB}"
     done
-    Tmp1=$1$i
-    Tmp2=${!Tmp1}
-    case $# in
-      1)
-        CombinAll="${CombinAll}&${Tmp2}"
-        ;;
-      2)
-        CombinAll="${CombinAll}&${Tmp2}@$2"
-        ;;
-      3)
-        if [ $(($i % 2)) -eq 1 ]; then
-          CombinAll="${CombinAll}&${Tmp2}@$2"
-        else
-          CombinAll="${CombinAll}&${Tmp2}@$3"
-        fi
-        ;;
-      4)
-        case $(($i % 3)) in
-          1)
-            CombinAll="${CombinAll}&${Tmp2}@$2"
-            ;;
-          2)
-            CombinAll="${CombinAll}&${Tmp2}@$3"
-            ;;
-          0)
-            CombinAll="${CombinAll}&${Tmp2}@$4"
-            ;;
-        esac
-        ;;
-    esac
-  done
-  echo ${CombinAll} | perl -pe "{s|^&||; s|^@+||; s|&@|&|g; s|@+|@|g}"
+    
+    for ((n=1; n<=${UserSum}; n++)); do
+      for num in ${TempBlockCookie}; do
+        [[ $n -eq $num ]] && continue 2
+      done
+      CombinAll="${CombinAll}&${ForOtherAll}"
+    done
+
+  else
+    for ((i=1; i<=${UserSum}; i++)); do
+      for num in ${TempBlockCookie}; do
+        [[ $i -eq $num ]] && continue 2
+      done
+      Tmp1=$1$i
+      Tmp2=${!Tmp1}
+      CombinAll="${CombinAll}&${Tmp2}"
+    done
+  fi
+
+  echo ${CombinAll} | perl -pe "{s|^&||; s|^@+||; s|&@|&|g; s|@+&|&|g; s|@+|@|g; s|@+$||}"
 }
 
 ## 组合Cookie、Token与互助码
 function Combin_All {
   export JD_COOKIE=$(Combin_Sub Cookie)
-  export JXNCTOKENS=$(Combin_Sub TokenJxnc)
-  #东东农场(jd_fruit.js)
   export FRUITSHARECODES=$(Combin_Sub ForOtherFruit)
-  #东东萌宠(jd_pet.js)
   export PETSHARECODES=$(Combin_Sub ForOtherPet)
-  #种豆得豆(jd_plantBean.js)
   export PLANT_BEAN_SHARECODES=$(Combin_Sub ForOtherBean)
-  #京喜工厂(jd_dreamFactory.js)
   export DREAM_FACTORY_SHARE_CODES=$(Combin_Sub ForOtherDreamFactory)
-  #东东工厂(jd_jdfactory.js)
   export DDFACTORY_SHARECODES=$(Combin_Sub ForOtherJdFactory)
-  #京东赚赚(jd_jdzz.js)
   export JDZZ_SHARECODES=$(Combin_Sub ForOtherJdzz)
-  #crazyJoy(jd_crazy_joy.js)
   export JDJOY_SHARECODES=$(Combin_Sub ForOtherJoy)
-  #惊喜农场(jd_jxnc.js)
   export JXNC_SHARECODES=$(Combin_Sub ForOtherJxnc)
-  #口袋书店(jd_bookshop.js)
+  export JXNCTOKENS=$(Combin_Sub TokenJxnc)
   export BOOKSHOP_SHARECODES=$(Combin_Sub ForOtherBookShop)
-  #签到领现金(jd_cash.js)
   export JD_CASH_SHARECODES=$(Combin_Sub ForOtherCash)
-  #闪购盲盒(jd_sgmh.js)
   export JDSGMH_SHARECODES=$(Combin_Sub ForOtherSgmh)
-  #环球挑战赛(jd_global.js)-活动时间：2021-02-02 至 2021-02-22
-  export JDGLOBAL_SHARECODES=$(Combin_Sub ForOtherGLOBAL)
+  export JDCFD_SHARECODES=$(Combin_Sub ForOtherCfd)
+  export JDGLOBAL_SHARECODES=$(Combin_Sub ForOtherGlobal)
 }
 
 ## 转换JD_BEAN_SIGN_STOP_NOTIFY或JD_BEAN_SIGN_NOTIFY_SIMPLE
@@ -155,9 +138,10 @@ function Help {
   echo -e "本脚本的用法为："
   echo -e "1. bash ${HelpJd} xxx      # 如果设置了随机延迟并且当时时间不在0-2、30-31、59分内，将随机延迟一定秒数"
   echo -e "2. bash ${HelpJd} xxx now  # 无论是否设置了随机延迟，均立即运行"
-  echo -e "3. bash ${HelpJd} hangup   # 重启挂机程序"
-  echo -e "4. bash ${HelpJd} resetpwd # 重置控制面板用户名和密码"
-  echo -e "\n针对用法1、用法2中的\"xxx\"，无需输入后缀\".js\"，另外，如果前缀是\"jd_\"的话前缀也可以省略。"
+  echo -e "3. bash ${HelpJd} runall   # 运行所有非挂机脚本，非常耗时"
+  echo -e "4. bash ${HelpJd} hangup   # 重启挂机程序"
+  echo -e "5. bash ${HelpJd} resetpwd # 重置控制面板用户名和密码"
+  echo -e "\n针对用法1、用法2中的\"xxx\"，可以不输入后缀\".js\"，另外，如果前缀是\"jd_\"的话前缀也可以省略。"
   echo -e "当前有以下脚本可以运行（仅列出以jd_、jr_、jx_、my_开头的脚本）："
   cd ${ScriptsDir}
   for ((i=0; i<${#ListScripts[*]}; i++)); do
@@ -168,41 +152,29 @@ function Help {
 
 ## nohup
 function Run_Nohup {
-  for js in ${HangUpJs}
-  do
-    if [[ $(ps -ef | grep "${js}" | grep -v "grep") != "" ]]; then
-      ps -ef | grep "${js}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-    fi
-  done
-
-  for js in ${HangUpJs}
-  do
-    [ ! -d ${LogDir}/${js} ] && mkdir -p ${LogDir}/${js}
-    LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
-    LogFile="${LogDir}/${js}/${LogTime}.log"
-    nohup node ${js}.js > ${LogFile} &
-  done
-}
-
-## pm2
-function Run_Pm2 {
-  pm2 flush
-  for js in ${HangUpJs}
-  do
-    pm2 restart ${js}.js || pm2 start ${js}.js
-  done
+  if [[ $(ps -ef | grep "${js}" | grep -v "grep") != "" ]]; then
+    ps -ef | grep "${js}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
+  fi
+  [ ! -d ${LogDir}/${js} ] && mkdir -p ${LogDir}/${js}
+  LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
+  LogFile="${LogDir}/${js}/${LogTime}.log"
+  nohup node ${js}.js > ${LogFile} &
 }
 
 ## 运行挂机脚本
 function Run_HangUp {
-  Import_Conf $1 && Detect_Cron && Set_Env
   HangUpJs="jd_crazy_joy_coin"
   cd ${ScriptsDir}
-  if type pm2 >/dev/null 2>&1; then
-    Run_Pm2 2>/dev/null
-  else
-    Run_Nohup >/dev/null 2>&1
-  fi
+  for js in ${HangUpJs}; do
+    Import_Conf ${js} && Set_Env
+    if type pm2 >/dev/null 2>&1; then
+      pm2 stop ${js}.js 2>/dev/null
+      pm2 flush
+      pm2 start -a ${js}.js --watch "${ScriptsDir}/${js}.js" --name="${js}"
+    else
+      Run_Nohup >/dev/null 2>&1
+    fi
+  done
 }
 
 ## 重置密码
@@ -211,13 +183,31 @@ function Reset_Pwd {
   echo -e "控制面板重置成功，用户名：admin，密码：adminadmin\n"
 }
 
-## 运行京东脚本
+## 一次性运行所有脚本
+function Run_All {
+  if [ ! -f ${ListJs} ]; then
+    cat ${ListCronLxk} | grep -E "j[drx]_\w+\.js" | perl -pe "s|.+(j[drx]_\w+)\.js.+|\1|" | sort -u > ${ListJs}
+  fi
+  echo -e "\n==================== 开始运行所有非挂机脚本 ====================\n"
+  echo -e "请注意：本过程将非常非常耗时，一个账号可能长达几小时，账号越多耗时越长，如果是手动运行，退出终端也将终止运行。\n"
+  echo -e "倒计时5秒...\n"
+  for ((sec=5; sec>0; sec--)); do
+    echo -e "$sec...\n"
+    sleep 1
+  done
+  for file in $(cat ${ListJs}); do
+    echo -e "==================== 运行 $file.js 脚本 ====================\n"
+    bash ${ShellJd} $file now
+  done
+}
+
+## 正常运行单个脚本
 function Run_Normal {
   Import_Conf $1 && Detect_Cron && Set_Env
   
   FileNameTmp1=$(echo $1 | perl -pe "s|\.js||")
   FileNameTmp2=$(echo $1 | perl -pe "{s|jd_||; s|\.js||; s|^|jd_|}")
-  SeekDir="${ScriptsDir} ${ScriptsDir}/backUp ${ConfigDir} ${OtherScriptsDir}"
+  SeekDir="${ScriptsDir} ${ScriptsDir}/backUp ${ConfigDir}"
   FileName=""
   WhichDir=""
 
@@ -241,7 +231,7 @@ function Run_Normal {
     LogFile="${LogDir}/${FileName}/${LogTime}.log"
     [ ! -d ${LogDir}/${FileName} ] && mkdir -p ${LogDir}/${FileName}
     cd ${WhichDir}
-    node ${FileName}.js | tee ${LogFile}
+    node ${FileName}.js 2>&1 | tee ${LogFile}
   else
     echo -e "\n在${ScriptsDir}、${ScriptsDir}/backUp、${ConfigDir}三个目录下均未检测到 $1 脚本的存在，请确认...\n"
     Help
@@ -255,21 +245,31 @@ case $# in
     Help
     ;;
   1)
-    if [[ $1 == hangup ]]; then
-      Run_HangUp
-    elif [[ $1 == resetpwd ]]; then
-      Reset_Pwd
-    else
-      Run_Normal $1
-    fi
+    case $1 in
+      hangup)
+        Run_HangUp
+        ;;
+      resetpwd)
+        Reset_Pwd
+        ;;
+      runall)
+        Run_All
+        ;;
+      *)
+        Run_Normal $1
+        ;;
+    esac
     ;;
   2)
-    if [[ $2 == now ]]; then
-      Run_Normal $1 $2
-    else
-      echo -e "\n命令输入错误...\n"
-      Help
-    fi
+    case $2 in
+      now)
+        Run_Normal $1 $2
+        ;;
+      *)
+        echo -e "\n命令输入错误...\n"
+        Help
+        ;;
+    esac
     ;;
   *)
     echo -e "\n命令过多...\n"
