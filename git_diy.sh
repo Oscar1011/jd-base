a09b769555dd7f201794a0ff33ee530fa5a6570f
Oscar1011 <shengbinxin@gmail.com>
2021-04-02 22:03:54 +0800

    Update git_diy.sh

--- git_diy.sh
+++ git_diy.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/env bash
+#author:spark thanks to: https://github.com/sparkssssssss/scripts
 
 ShellDir=/jd
 
 source ${ShellDir}/notify.sh
 
-git_url=$1
-git_branch=$2
-author=$3
-diyscripts=/jd/alljs/${author}
+declare -A BlackListDict
+author=$1
+repo=$2
+path=$3
+blackword=$4
 
-if [ ! -d ${diyscripts} ]; then
-  git clone -b ${git_branch} ${git_url} ${diyscripts}
+if [[ $# -lt 2 ]] || [[ $# -gt 4 ]] ; then
+  echo 'Desc: 用户拉取指定用户的指定仓储'
+  echo 'Usage: diy <auth> <repo> <path> <blacklist>'
+
+  echo 'auth  作者名'
+  echo 'repo  仓储名'
+  echo 'path  需要下载脚本的指定目录，多个目录 | 分割  path1 | path2'
+  echo 'blacklist  需要排除的脚本名，多个名称 | 分割  blacklist1 | blacklist2'
+  exit 0;
 fi
 
-cd ${diyscripts}
-git pull
-gitpullstatus=$?
-git reset --hard origin/${git_branch}
+diyscriptsdir=/jd/alljs
+mkdir -p ${diyscriptsdir}
 
+if [ ! -d "$diyscriptsdir/${author}_${repo}" ]; then
+  echo -e "${author}本地仓库不存在,从gayhub拉取ing..."
+  cd ${diyscriptsdir} &&  git clone https://github.com.cnpmjs.org/${author}/${repo}.git ${author}_${repo}
+  gitpullstatus=$?
+  [ $gitpullstatus -eq 0 ] && echo -e "${author}本地仓库拉取完毕"
+  [ $gitpullstatus -ne 0 ] && echo -e "${author}本地仓库拉取失败,请检查!" && exit 0
+else
+  cd ${diyscriptsdir}/${author}_${repo}
+  branch=`git symbolic-ref --short -q HEAD`
+  git fetch --all
+  git reset --hard origin/$branch
+  git pull
+  gitpullstatus=$?
+fi
 
 rand(){
     min=$1
@@ -25,54 +46,65 @@ rand(){
     num=$(cat /proc/sys/kernel/random/uuid | cksum | awk -F ' ' '{print $1}')
     echo $(($num%$max+$min))
 }
+
 function updatefile {
-  cd ${diyscripts}
+  cd ${diyscriptsdir}/${author}_${repo}
   for js in `ls *.js`;
     do
      cp $js /jd/scripts/${author}_$js
   done
 }
+
+
 function addnewcron {
   addname=""
-  cd ${diyscripts}
-  for js in `ls *.js`;
+  cd ${diyscriptsdir}/${author}_${repo}
+  express=$(find . -name "*.js")
+  if [ $path ];then
+    express=$(find . -name "*.js" | egrep $path)
+  fi
+  if [ $blackword ];then
+    express=$(find . -name "*.js" | egrep -v $blackword | egrep $path)
+  fi
+  for js in $express;
     do 
-      croname=`echo "${author}_$js"|awk -F\. '{print $1}'`
-      script_date=`cat  $js|grep ^[0-9]|awk '{print $1,$2,$3,$4,$5}'|sort |uniq|head -n 1`
+      base=`basename $js`
+      croname=`echo "${author}_$base"|awk -F\. '{print $1}'`
+      script_date=`cat  $js|grep ^[0-9]|awk '{print $1,$2,$3,$4,$5}'|egrep -v "[a-zA-Z]|:|\."|sort |uniq|head -n 1`
       [ -z "${script_date}" ] && script_date=`cat  $js|grep -Eo "([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9][,-].*)"|sort |uniq|head -n 1`
       [ -z "${script_date}" ] && cron_min=$(rand 1 59) && cron_hour=$(rand 7 9) && script_date="${cron_min} ${cron_hour} * * *"
-      [ $(grep -c "$croname" /jd/config/crontab.list) -eq 0 ] && sed -i "/\#diy/a${script_date} bash jd $croname"  /jd/config/crontab.list && \cp $js /jd/scripts/${author}_$js && addname="${addname}\n${croname}"
+      [ $(grep -c -w "$croname" /jd/config/crontab.list) -eq 0 ] && sed -i "/hangup/a${script_date} js $croname"  /jd/config/crontab.list && addname="${addname}\n${croname}" && echo -e "添加了新的脚本${croname}."
+      if [ ! -f "/jd/scripts/${author}_$base" ];then
+        \cp $js /jd/scripts/${author}_$base
+      else
+        change=$(diff $js /jd/scripts/${author}_$base)
+        [ -n "${change}" ] && \cp $js /jd/scripts/${author}_$base && echo -e "${author}_$base 脚本更新了."
+      fi
   done
-
-  if [ "$addname" != "" ]; then
-      echo -e "成功添加新的定时任务：\n\n${addname}" > ${ContentNewTask}
-      Notify_NewTask
-  fi
-  #[ "$addname" != "" ] && /bin/sh  /jd/scripts/wechat.sh "${author}新增自定义脚本" "${addname}"
+  [ "$addname" != "" ] && notify "新增 ${author} 自定义脚本" "${addname}"
 
 }
 
 function delcron {
   delname=""
-  cronfiles=$(grep "$author" /jd/config/crontab.list|awk -F"${author}_" '{print $2}')
+  cronfiles=$(grep "$author" /jd/config/crontab.list|grep -v "^#"|awk '{print $8}'|awk -F"${author}_" '{print $2}')
   for filename in $cronfiles;
     do
-      if [ ! -f "${diyscripts}/${filename}.js" ]; then 
-        sed -i "/bash jd ${author}_${filename}/d" /jd/config/crontab.list 
+      if [ ! -f "${diyscriptsdir}/${author}_${repo}/${filename}.js" ]; then 
+        sed -i "/\<js ${author}_${filename}\>/d" /jd/config/crontab.list && echo -e "删除失效脚本${filename}."
 	      delname="${delname}\n${author}_${filename}"
       fi
   done
-  if [ "$delname" != "" ]; then
-      echo -e "删除失效的定时任务：\n\n${delname}" > ${ContentDropTask}
-      Notify_DropTask
-  fi
-  #[ "$delname" != "" ] && /bin/sh  /jd/scripts/wechat.sh "${author}删除失效脚本" "${delname}"    
+  [ "$delname" != "" ] && notify  "删除 ${author} 失效脚本" "${delname}" 
 }
 
 if [[ ${gitpullstatus} -eq 0 ]]
 then
-  updatefile
   addnewcron
   delcron
+else
+  echo -e "$author 仓库更新失败了."
+  notify "自定义仓库更新失败" "$author"
 fi
 
+exit 0
